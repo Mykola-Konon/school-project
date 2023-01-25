0fa51e62ae961af154d623751897fcf630efb2d6
Mykola Konon <mykolakonon@gmail.com>
2023-01-25 21:29:05 +0200

    Update lesson-2.py

---
+++ lesson-2.py
@@ -0,0 +1,66 @@
+# змінення розміру букв
+# message = "hello alis"
+# print("Title: ", message.title())
+# print("Title: ", message.upper())
+
+# new_message = "heSASlo SADDFis"
+# print("Title: ", new_message.lower())
+
+# first_name = "Lisa"
+# last_name = "Simpson"
+# full_name = first_name + " " + last_name
+
+# print("Hello, ", full_name.title())
+# email = first_name.lower() + last_name.lower() + "company.com"
+# print(email)
+
+# num1 = 5
+# num2 = 3
+# sum_ = num1 + num2
+
+# print(f"The sum of {num1} + {num2} is equel {sum_}")
+
+
+# num1 = input("Enter first number: ")
+# num2 = input("Ener second number: ")
+# sum_ = float(num1) + float(num2)
+
+# print(f"The sum of {num1} + {num2} is equel {sum_}")
+
+
+# num = float(input("Enter your number: "))
+
+# num_sqt = num ** 0.5
+# print("The sqrt of %0.3f is equal %0.3f"%(num, num_sqt))
+
+
+# import cmath
+# num = 1 + 6j
+# num_sqt = cmath.sqrt(num)
+# print("The esqure root of {0} is {1:.3f} + {2:.4f}".format(num, num_sqt, num_sqt.imag ))
+
+#
+# a = float(input("Enter 'a': "))
+# b = float(input("Enter 'b': "))
+# c = float(input("Enter 'c': "))
+
+# p = (a + b + c) / 2
+# s = (p * (p - a) * (p - b) * (p - c)) ** 0.5
+# print(f"The perimetr is eqal {p}")
+# print(f"The area is esqel {s}")
+
+# quadratic equation
+
+# import cmath
+
+
+# a = float(input("Enter 'a': "))
+# b = float(input("Enter 'b': "))
+# c = float(input("Enter 'c': "))
+
+# d = (b**2) - (4 * a * c)
+
+# result_m = (-b-cmath.sqrt(d)) / (2 * a)
+# result_p = (-b-cmath.sqrt(d)) / (2 * a)
+
+# print(f"Results are {result_m} and {result_p}")
